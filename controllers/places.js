7b9aebb7746f56d01337462e6ba50e9908be5d5a
ambertua <amber.tuamoheloa@yahoo.com>
2023-04-22 13:25:13 -0700

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,48 +1,63 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const places = require("../models/places.js");
 
-// GET /places
+// show route
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
 
+
+//new route
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
+// index route
 router.get("/", (req, res) => {
-  let places = [];
   res.render("places/index", { places });
 });
 
-let places = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "http://placekitten.com/250/250",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "http://placekitten.com/250/250",
-  },
-];
+router.post("/", (req, res) => {
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+
+
+// Export the router
 module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-require("dotenv").config();
-const express = require("express");
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-
-app.use("/places", require("./controllers/places"));
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get("/", (req, res) => {
   res.render("home");
